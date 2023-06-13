3242b75db86ed331ee37b3e395da0174e141b8c0
Vishnu K <64530007+VishDroid-dev@users.noreply.github.com>
2023-06-13 15:01:08 +0530

    Update page.tsx

--- app/videos/[slug]/page.tsx
+++ app/videos/[slug]/page.tsx
@@ -5,7 +5,7 @@ interface VideosListProps {
 export default function VideosListPage({slug}: VideosListProps) {
     return (
         <div>
-            <h1 className="text-3xl text-white">{props.slug}</h1>
+            <h1 className="text-3xl text-white">{slug}</h1>
         </div>
     );
 }

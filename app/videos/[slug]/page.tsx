5d25891746465f58643fc2f7c37e25ba2cc9d805
VishDroid-dev <vk2345732@gmail.com>
2023-06-14 19:35:01 +0530

    fixed the bug

--- app/videos/[slug]/page.tsx
+++ app/videos/[slug]/page.tsx
@@ -1,111 +1,111 @@
 "use client"
 
-import { useEffect, useState } from "react"
-import { YoutubeVideos, webExploitation } from "@/data/web-exploitation"
-import firebase from "@/firebaseConfig"
-import { useCollection } from "react-firebase-hooks/firestore"
+import { useEffect, useState } from "react";
+import firebase from "@/firebaseConfig";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { Input } from "@/components/ui/input";
+import { VideoCard } from "@/components/videocard";
 
-import { Input } from "@/components/ui/input"
-import { VideoCard } from "@/components/videocard"
+interface Video {
+  title: string;
+  link: string;
+}
 
 interface VideosListProps {
   params: {
-    slug: string
-  }
+    slug: string;
+  };
 }
 
-export default async function VideosListPage({ params }: VideosListProps) {
-  const [title, setTitle] = useState<string | null>(null)
-  const [search, setSearch] = useState<string>("")
-  const [videoData, setVideoData] = useState<
-    {
-      title: string
-      youtube: string
-    }[]
-  >([])
+export default function VideosListPage({ params }: VideosListProps) {
+  const [title, setTitle] = useState<string | null>(null);
+  const [search, setSearch] = useState<string>("");
+  const [videoData, setVideoData] = useState<Video[]>([]);
+  const [filteredVideoData, setFilteredVideoData] = useState<Video[]>([]);
 
   const fetchVideosByCategory = async (category: string) => {
-    const db = firebase.firestore()
+    const db = firebase.firestore();
     try {
-      // Specify the collection and query based on the category field
       const querySnapshot = await db
         .collection("videos")
         .where("category", "==", category)
-        .get()
+        .get();
 
-      // Retrieve the list of documents matching the category
-      const videos: any[] = []
+      const videos: Video[] = [];
       querySnapshot.forEach((doc) => {
-        console.log("doc: ", doc)
-        videos.push(doc.data())
-      })
+        videos.push(doc.data() as Video);
+      });
 
-      console.log("Videos fetched from Firestore:", videos)
-      return videos
+      return videos;
     } catch (error) {
-      console.error("Error fetching videos:", error)
-      return []
+      console.error("Error fetching videos:", error);
+      return [];
     }
-  }
+  };
 
-  const { slug } = params
-
+  const { slug } = params;
 
   useEffect(() => {
     const fetchVideos = async () => {
+      let categoryTitle = null;
+      let categoryValue = "";
+
       if (slug === "web-exploitation") {
-        setTitle("Web Exploitation");
-        const videos = await fetchVideosByCategory("web-exploitation");
-        setVideoData(videos);
+        categoryTitle = "Web Exploitation";
+        categoryValue = "web-exploitation";
       } else if (slug === "binary-exploitation") {
-        setTitle("Binary Exploitation");
-        const videos = await fetchVideosByCategory("binary-exploitation");
-        setVideoData(videos);
+        categoryTitle = "Binary Exploitation";
+        categoryValue = "binary-exploitation";
       } else if (slug === "network-forensics") {
-        setTitle("Network Forensics");
-        const videos = await fetchVideosByCategory("network-forensics");
-        setVideoData(videos);
-      } else {
-        setTitle(null);
-        setVideoData([]);
+        categoryTitle = "Network Forensics";
+        categoryValue = "network-forensics";
       }
+
+      setTitle(categoryTitle);
+      const videos = await fetchVideosByCategory(categoryValue);
+      setVideoData(videos);
     };
 
     fetchVideos();
   }, [slug]);
 
+  useEffect(() => {
+    const filterVideos = () => {
+      const filteredVideos = videoData.filter((video) =>
+        video.title.toLowerCase().includes(search.toLowerCase())
+      );
+      setFilteredVideoData(filteredVideos);
+    };
 
+    filterVideos();
+  }, [search, videoData]);
 
-  function extractYouTubeVideoId(url: string) {
+  const extractYouTubeVideoId = (url: string): string | null => {
     const videoIdRegex =
-      /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/)|youtu\.be\/|youtube\.com\/(?:v\/|embed\/|watch\?v=))([\w-]{11})(?:\S+)?$/
-    const match = url.match(videoIdRegex)
-    if (match && match[1]) {
-      return match[1]
-    } else {
-      return null
-    }
-  }
+      /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/)|youtu\.be\/|youtube\.com\/(?:v\/|embed\/|watch\?v=))([\w-]{11})(?:\S+)?$/;
+    const match = url.match(videoIdRegex);
+    return match && match[1] ? match[1] : null;
+  };
 
-  const GetAvatar = async (link: string): Promise<string | null> => {
-    const videoId = extractYouTubeVideoId(link)
+  const getAvatar = async (link: string): Promise<string | null> => {
+    const videoId = extractYouTubeVideoId(link);
     try {
       const avatarImgResponse = await fetch(
         `https://img.youtube.com/vi/${videoId}/0.jpg`
-      )
+      );
       if (avatarImgResponse.ok) {
-        return avatarImgResponse.url
+        return avatarImgResponse.url;
       } else {
         console.error(
           `Failed to fetch avatar image: ${avatarImgResponse.status} ${avatarImgResponse.statusText}`
-        )
-        return null
+        );
+        return null;
       }
     } catch (error) {
-      console.error("Failed to fetch avatar image:", error)
-      return null
+      console.error("Failed to fetch avatar image:", error);
+      return null;
     }
-  }
+  };
 
   return (
     <div className="w-full">
@@ -121,13 +121,10 @@ export default async function VideosListPage({ params }: VideosListProps) {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 h-full gap-4">
-        {videoData.map((video, i) => {
-          // console.log("video: ", video)
-          return (
-            <VideoCard key={i} title={video.title} youtube={video.youtube} />
-          )
-        })}
+        {filteredVideoData.map((video, i) => (
+          <VideoCard key={i} title={video.title} youtube={video.link} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
---
+++ data/videos.ts
@@ -0,0 +1,27 @@
+
+export const Videos = [
+    {
+        name: 'Video 1',
+        url: 'https://www.youtube.com/embed/1X7fZoDs9KU'
+    },
+    {
+        name: 'Video 2',
+        url: 'https://www.youtube.com/embed/1X7fZoDs9KU'
+    },
+    {
+        name: 'Video 3',
+        url: 'https://www.youtube.com/embed/1X7fZoDs9KU'
+    },
+    {
+        name: 'Video 4',
+        url: 'https://www.youtube.com/embed/1X7fZoDs9KU'
+    },
+    {
+        name: 'Video 5',
+        url: 'https://www.youtube.com/embed/1X7fZoDs9KU'
+    },
+    {
+        name: 'Video 6',
+        url: 'https://www.youtube.com/embed/1X7fZoDs9KU'
+    },
+]
